ce5758340787716a3ab99ce5014acf608b621f89
lwmfjc <lwmfjc@gmail.com>
2021-06-20 23:27:44 +0800

    "部分修改"

--- src/main/java/com/ly/Main.java
+++ src/main/java/com/ly/Main.java
@@ -1,10 +1,19 @@
 package com.ly;
 
 import com.ly.utils.Utils;
+import edu.princeton.cs.algs4.Date;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello ");
-        System.out.println(Utils.gcd(24,30));
+        //System.out.println(Utils.gcd(24,30));
+        int[] a=new int[]{1,2,3};
+        changeArray(a);
+        System.out.println(a.length);
+        //334232
+    }
+    private static void changeArray(int[] b){
+        b=new int[]{2,3};
     }
 }
