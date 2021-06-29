bf763e4ebc25a4cb69e908991ee81cf4e0bf399e
lwmfjc <lwmfjc@gmail.com>
2021-06-29 18:00:01 +0800

    '连通代码'

---
+++ src/main/java/com/ly/ch1/UF.java
@@ -0,0 +1,57 @@
+package com.ly.ch1;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * union-find的实现
+ */
+public class UF {
+    private int[] id; //分量id(以触点作为索引)
+    private int count;//分量数量
+    public UF(int N){
+        count=N;
+        id=new int[N];
+        for(int i=0;i<N;i++){
+            id[i]=i;
+        }
+    }
+    public int count(){
+        return count;
+    }
+    public boolean connected(int p,int q){
+        return find(p)==find(q);
+    }
+    public int find(int p){
+        return id[p];
+    }
+    public void union(int p,int q){
+        int pID=find(p);
+        int qID=find(q);
+        //如果p和q已经在相同分量之中则不需要采取任何行动
+        if(pID==qID){
+            return;
+        }
+        for(int i=0;i<id.length;i++){
+            if(id[i]==pID){
+                id[i]=qID;
+            }
+        }
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int N= StdIn.readInt();
+        UF uf=new UF(N);
+        while (!StdIn.isEmpty()){
+            int p=StdIn.readInt();
+            int q=StdIn.readInt();
+            if(uf.connected(p,q)){
+                continue;
+            }
+            uf.union(p,q);
+            StdOut.println(p+" "+q);
+        }
+        StdOut.println(uf.count()+"components");
+    }
+}
