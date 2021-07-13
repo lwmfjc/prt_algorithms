55fec099f54f8a5838979a7fc7f27a05b73c641f
lwmfjc <lwmfjc@gmail.com>
2021-07-13 23:29:27 +0800

    '练习类'

---
+++ src/main/java/com/ly/ch3/Test.java
@@ -0,0 +1,12 @@
+package com.ly.ch3;
+
+public class Test implements Comparable<Test> {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public int compareTo(Test o) {
+        return 0;
+    }
+}
