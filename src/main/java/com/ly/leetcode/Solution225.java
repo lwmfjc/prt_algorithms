0a2130daad421487e1144625cfae57fad923e2d5
lwmfjc <lwmfjc@gmail.com>
2021-07-14 23:02:14 +0800

    'leetcode习题'

---
+++ src/main/java/com/ly/leetcode/Solution225.java
@@ -0,0 +1,55 @@
+package com.ly.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * push to back、peek/pop from front、size 和 is empty
+ */
+public class Solution225 {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Solution225() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue1.poll();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue1.peek();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
