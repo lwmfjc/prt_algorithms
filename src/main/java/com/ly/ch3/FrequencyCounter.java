014e4f9136db45a1d7766205c5f4dbb5f4087880
lwmfjc <lwmfjc@gmail.com>
2021-07-16 09:24:02 +0800

    '练习用例'

--- src/main/java/com/ly/ch3/FrequencyCounter.java
+++ src/main/java/com/ly/ch3/FrequencyCounter.java
@@ -6,22 +6,16 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class FrequencyCounter {
     public static void main(String[] args) {
-        int minlen = Integer.parseInt(args[0]);//最小键长
-        ST<String, Integer> st = new ST<>();
+        int minlen = Integer.parseInt(args[0]);
+        ST<String, Integer> st = new ST<String, Integer>();
         while (!StdIn.isEmpty()) {
             String word = StdIn.readString();
-            //忽略较短的单词
-            if (word.length() < minlen) continue;
-            //不存在的
-            if (!st.contains(word)) {
-                st.put(word, 1);
-            } else {
-                st.put(word, st.get(word) + 1);
-            }
+            if (word.length() < minlen) continue;//忽略较短单词
+            if (!st.contains(word)) st.put(word, 1);
+            else st.put(word, st.get(word) + 1);
         }
-        System.out.println("数据准备完毕,开始查找----");
-        //找出频率最高的单词
-        String max = " ";
+        //找出出现频率最高的单词
+        String max = "";
         st.put(max, 0);
         for (String word : st.keys()) {
             if (st.get(word) > st.get(max)) {
---
+++ src/main/java/com/ly/ch3_exp/FrequencyCounter.java
@@ -0,0 +1,29 @@
+package com.ly.ch3_exp;
+
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdIn;
+
+public class FrequencyCounter {
+    public static void main(String[] args) {
+        int minlen = Integer.parseInt(args[0]);
+        ST<String, Integer> st = new ST<>();
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if(word.length()<minlen) continue;
+            if (st.get(word) != null) {
+                st.put(word, st.get(word) + 1);
+            } else {
+                st.put(word, 1);
+            }
+        }
+        String max = "";
+        //key可以为"",不能为null
+        st.put(max, 0);
+        for (String key : st.keys()) {
+            if (st.get(key) > st.get(max)) {
+                max = key;
+            }
+        }
+        System.out.printf("[%s]--[%d]", max, st.get(max));
+    }
+}
