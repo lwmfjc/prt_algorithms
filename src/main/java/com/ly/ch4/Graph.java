50456449a463b45d6ffbc878e2f8d4ce1a13d721
lwmfjc <lwmfjc@gmail.com>
2021-07-25 19:19:24 +0800

    '图的其他方法'

--- src/main/java/com/ly/ch4/Graph.java
+++ src/main/java/com/ly/ch4/Graph.java
@@ -4,6 +4,8 @@ package com.ly.ch4;
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
 
+import java.util.ArrayList;
+
 public class Graph {
     private final int V;//顶点数目
     private int E;//边的数目
@@ -46,5 +48,51 @@ public class Graph {
 
     public Iterable<Integer> adj(int v) {
         return adj[v];
+    }
+
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v)) {
+            degree++;
+        }
+        return degree;
+    }
+
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.V(); v++) {
+            //查找每个顶点的度并进行比较
+            if (degree(G, v) > max) {
+                max = degree(G, v);
+            }
+        }
+        return max;
+    }
+
+    //所有顶点的平均度数
+    public static double avgDegree(Graph G) {
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++) {
+            for (int w : G.adj(v)) {
+                if (v == w) count++;
+            }
+        }
+        return count / 2;
+    }
+
+    public String toString() {
+        String s = V + " vertices, " + E + " edges\n";
+        for (int v = 0; v < V; v++) {
+            s += v + ":";
+            for (int w : this.adj(v)) {
+                s += w + " ";
+            }
+            s += "\n";
+        }
+        return s;
     }
 }
---
+++ src/main/java/com/ly/ch4/TestSearch.java
@@ -0,0 +1,10 @@
+package com.ly.ch4;
+
+import edu.princeton.cs.algs4.In;
+
+public class TestSearch {
+    public static void main(String[] args) {
+        Graph G=new Graph(new In(args[0]));
+        int s=Integer.parseInt(args[1]);
+    }
+}
