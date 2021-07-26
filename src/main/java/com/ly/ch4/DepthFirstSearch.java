09b99e96d57779faa0f0fe89c051ce2d1f3d538d
lwmfjc <lwmfjc@gmail.com>
2021-07-26 11:30:21 +0800

    '深度优先搜索'

---
+++ src/main/java/com/ly/ch4/DepthFirstSearch.java
@@ -0,0 +1,31 @@
+package com.ly.ch4;
+
+public class DepthFirstSearch {
+    private boolean[] marked;
+    private int count;
+
+    public DepthFirstSearch(Graph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;//当前节点搜索过了
+        //System.out.printf("%3d", v);
+        count++;
+        for (int w : G.adj(v)) {
+            //如果w没有搜索过,那就继续搜索
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean marked(int w) {
+        return marked[w];
+    }
+
+    public int count() {
+        return count;
+    }
+}
--- src/main/java/com/ly/ch4/Main.java
+++ src/main/java/com/ly/ch4/Main.java
@@ -1,4 +1,24 @@
 package com.ly.ch4;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Main {
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        int s = Integer.parseInt(args[1]);
+        DepthFirstSearch
+                search = new DepthFirstSearch(G, s);
+        //搜索和s相连的所有节点个数
+        for (int v = 0; v < G.V(); v++) {
+            if (search.marked(v)) {
+                StdOut.print(v + " ");
+            }
+        }
+        StdOut.println();
+        if (search.count() != G.V()) {
+            StdOut.print("NOT ");
+        }
+        StdOut.println("connected");
+    }
 }
