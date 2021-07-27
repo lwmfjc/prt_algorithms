2d0fa152745d64c554417dc3e8eddb64a0dce616
lwmfjc <lwmfjc@gmail.com>
2021-07-27 17:51:57 +0800

    '有向图的可达性'

---
+++ src/main/java/com/ly/ch4/_42/DirectedDFS.java
@@ -0,0 +1,53 @@
+package com.ly.ch4._42;
+
+import com.ly.ch4._41.Graph;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 有向图的可达性
+ */
+public class DirectedDFS {
+    private boolean[] marked;
+
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    public DirectedDFS(Digraph G, Bag<Integer> sources) {
+        marked = new boolean[G.V()];
+        for (int v : sources) {
+            dfs(G, v);
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean marked(int v) {
+        return marked[v];
+    }
+
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+        Bag<Integer> sources = new Bag<>();
+        for (int i = 1; i < args.length; i++) {
+            sources.add(Integer.parseInt(args[i]));
+        }
+        DirectedDFS reachable = new DirectedDFS(G, sources);
+        for (int v = 0; v < G.V(); v++) {
+            if (reachable.marked(v)) {
+                StdOut.print(v + " ");
+            }
+        }
+        StdOut.println();
+    }
+}
