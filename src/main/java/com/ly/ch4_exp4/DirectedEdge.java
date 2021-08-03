7dfc4c9ba2f8ab04cc9ae9533fb2399947ebc219
lwmfjc <lwmfjc@gmail.com>
2021-08-03 16:47:27 +0800

    '有向权重图'

---
+++ src/main/java/com/ly/ch4_exp4/DirectedEdge.java
@@ -0,0 +1,34 @@
+package com.ly.ch4_exp4;
+
+public class DirectedEdge {
+    private final int from;
+    private final int to;
+    private final double weight;
+
+    public DirectedEdge(int from, int to, double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public int from() {
+        return from;
+    }
+
+    public int to() {
+        return to;
+    }
+
+    public double weight(){
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectedEdge{" +
+                "from=" + from +
+                ", to=" + to +
+                ", weight=" + weight +
+                '}';
+    }
+}
---
+++ src/main/java/com/ly/ch4_exp4/EdgeWeightedDigraph.java
@@ -0,0 +1,67 @@
+package com.ly.ch4_exp4;
+
+
+import com.ly.ch1.Bag;
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.In;
+
+import java.util.Arrays;
+
+public class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<>();
+        }
+    }
+
+    public EdgeWeightedDigraph(In in) {
+        this(in.readInt());
+        int e = in.readInt();
+        for (int n = 0; n < e; n++) {
+            int from = in.readInt();
+            int to = in.readInt();
+            double weight = in.readDouble();
+            addEdge(new DirectedEdge(from, to, weight));
+        }
+    }
+
+    private void addEdge(DirectedEdge e) {
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v) {
+        return adj[v];
+    }
+
+    @Override
+    public String toString() {
+        String s = "EdgeWeightedDigraph{" +
+                "V=" + V +
+                ", E=" + E + "}\n";
+        for (int v = 0; v < V; v++) {
+            s += v + ":";
+            for (DirectedEdge e : adj(v)) {
+                int from = e.from();
+                int to = e.to();
+                double weight = e.weight();
+                s += " [" + from + "->" + to +"("+weight+ ")] ";
+            }
+            s += "\n";
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        EdgeWeightedDigraph G =
+                new EdgeWeightedDigraph(new In(args[0]));
+        System.out.println(G);
+    }
+}
