81cd246a8072f8cb0b45006c8351f6be582f3485
lwmfjc <lwmfjc@gmail.com>
2021-08-03 10:27:32 +0800

    有向无环图的判断以及拓扑排序的处理

--- src/main/java/com/ly/ch4_exp2/DepthFirstOrder.java
+++ src/main/java/com/ly/ch4_exp2/DepthFirstOrder.java
@@ -1,4 +1,60 @@
 package com.ly.ch4_exp2;
 
+import com.ly.ch1.Queue;
+import com.ly.ch1.Stack;
+import edu.princeton.cs.algs4.In;
+
+//基于深度优先搜索的顶点排序
 public class DepthFirstOrder {
+    private boolean[] marked;
+    private Queue<Integer> pre;//前序排序
+    private Queue<Integer> post;//后序排序
+    private Stack<Integer> reversePost;//逆后序排序
+
+    public DepthFirstOrder(Digraph G) {
+        marked = new boolean[G.V()];
+        pre = new Queue<>();
+        post = new Queue<>();
+        reversePost = new Stack<>();
+        for (int s = 0; s < G.V(); s++) {
+            if (!marked[s]) {
+                dfs(G, s);
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        pre.enqueue(v);
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+        post.enqueue(v);
+        reversePost.push(v);
+    }
+
+    public Iterable<Integer> pre() {
+        return pre;
+    }
+
+    public Iterable<Integer> post() {
+        return post;
+    }
+
+    public Iterable<Integer> reversePost() {
+        return reversePost;
+    }
+
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+        //System.out.println(G);
+        //System.out.println();
+        DepthFirstOrder order = new DepthFirstOrder(G);
+        for (int i : order.reversePost()) {
+            System.out.print(i + " ");
+        }
+    }
+
 }
--- src/main/java/com/ly/ch4_exp2/DirectedCycle.java
+++ src/main/java/com/ly/ch4_exp2/DirectedCycle.java
@@ -1,4 +1,66 @@
 package com.ly.ch4_exp2;
 
+import com.ly.ch1.Stack;
+import com.ly.ch4_exp1.Graph;
+import edu.princeton.cs.algs4.In;
+
+//检查有向图是否存在环
 public class DirectedCycle {
+    private boolean[] onStack;
+    private boolean[] marked;
+    private Stack<Integer> cycle;
+    private int[] edgeTo;//记录路径
+    private boolean hasCycle;//存在环
+
+    public DirectedCycle(Digraph G) {
+        onStack = new boolean[G.V()];
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        for (int v = 0; v < G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        onStack[v] = true;
+        for (int w : G.adj(v)) {
+            if (this.hasCycle()) {
+                return;
+            }else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            } else if (onStack[w]) {
+                cycle=new Stack<>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+
+    public static void main(String[] args) {
+        Digraph G = new Digraph(new In(args[0]));
+        DirectedCycle cycle=new DirectedCycle(G);
+        if(cycle.hasCycle()){
+            final Iterable<Integer> cycle1 = cycle.cycle();
+            for(int i:cycle1){
+                System.out.print(i+" ");
+            }
+        }
+    }
+
 }
