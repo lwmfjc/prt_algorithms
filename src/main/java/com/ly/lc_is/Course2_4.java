3193f2c288679c1e855eca999765b665ceba7bee
lwmfjc <lwmfjc@gmail.com>
2022-03-22 10:40:32 +0800

    二分查找白名单

---
+++ src/main/java/com/ly/lc_is/Course2_4.java
@@ -0,0 +1,21 @@
+package com.ly.lc_is;
+
+public class Course2_4 {
+    /**
+     * 查找一个数在数组中该插入的位置
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int searchInsert(int[] nums,int target){
+        if(nums==null){
+            return 0;
+        }
+        int low=0,high=nums.length;
+        //while (low<=)
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
