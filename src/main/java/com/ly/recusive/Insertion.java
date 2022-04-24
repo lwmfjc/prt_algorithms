e70d23b6c3bd2425c4b92c832b375d085c3478c9
lwmfjc <lwmfjc@gmail.com>
2022-04-24 17:31:19 +0800

    插入排序

--- src/main/java/com/ly/recusive/Insertion.java
+++ src/main/java/com/ly/recusive/Insertion.java
@@ -5,13 +5,13 @@ import edu.princeton.cs.algs4.StdIn;
 public class Insertion extends BaseSort {
 
     public static void sort(Comparable[] a) {
-        int N=a.length;
+        int N = a.length;
         //将下表为 n-1的数，依次和n-2,n-3一直到0比较，
         //所以第二层for只走到1，因为0前面没有值
         //如果比前面的值小，就进行交换
-        for(int i=1;i<N;i++){
-            for(int j=i;j>0&&less(a[j],a[j-1]);j--){
-                exch(a,j,j-1);
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
             }
         }
     }
