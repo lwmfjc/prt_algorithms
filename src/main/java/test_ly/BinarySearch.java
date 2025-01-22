8af878c47b1c6a3b1337b2adda068e7f09c7bce6
lwmfjc <lwmfjc@gmail.com>
2025-01-22 09:10:19 +0800

    复习

---
+++ src/main/java/test_ly/BinarySearch.java
@@ -0,0 +1,36 @@
+package test_ly;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+    
+    public static void main(String[ ] args){
+        //读取文件中的值(白名单)
+        int[] whitelist= In.readInts(args[0]);
+        Arrays.sort(whitelist);
+        //如果不是空
+        while(!StdIn.isEmpty()){
+            //那么就读取一个整数
+            int key=StdIn.readInt();
+            //如果输入的值不在文件中
+            if(rank(key,whitelist)==-1){
+                StdOut.println(key);
+            }
+        }
+    }
+}
